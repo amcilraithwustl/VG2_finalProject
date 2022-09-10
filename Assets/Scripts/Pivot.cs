ec9def391b62b03223330de3a5c54e43f8d37e4c
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-09-10 15:25:27 -0500

    Fixing pivot

---
+++ Assets/Scripts/Pivot.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Composites;
+
+public class Pivot : MonoBehaviour
+{
+    private Transform _rigidbody;
+    private float _rotationDirection = 0;
+    public float rotationSpeed = 10;
+    void Start()
+    {
+        _rigidbody = GetComponent<Transform>();
+        
+    }
+
+    public void FixedUpdate()
+    {
+        Debug.Log("DING " + (_rotationDirection * rotationSpeed));
+        _rigidbody.Rotate(new Vector3(0, 0, 1), _rotationDirection * rotationSpeed);
+        // _rigidbody.Rotate(new Vector3(0, 0, 1), 1);
+
+    }
+
+    public void Fire(InputAction.CallbackContext ctx)
+    {
+        _rotationDirection = ctx.ReadValue<float>();
+    
+    }
+    
+    
+}
